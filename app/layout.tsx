968441442421344b6af831dd7c07b2ba8b48a16f
Miao-zheng-wei <78770776+Miao-zheng-wei@users.noreply.github.com>
2024-07-30 16:52:57 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "鸭虎",
+  description: "",
   appleWebApp: {
     title: "NextChat",
     statusBarStyle: "default",
